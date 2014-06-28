e06a8489210f630e94053744618640f699c753b1
GlyphGryph <glyphgryph@gmail.com>
2014-06-27 18:08:18 -0700

    turned 'optional' arguments into a correct options hash setup.

--- src/game.js
+++ src/game.js
@@ -2,9 +2,18 @@ function Game(){
   var that = this;
   var character = new Character("Jake");
 
-  var candy = new Happening("Some candy is available.", []);
-  var new_table = new Happening("You check out a new table.", [], {}, candy)
-  var throwup = new Happening("You eat some candy and then throw up.", [], {"lose_health": 10}, new_table);
+  var candy = new Happening({
+    "description": "Some candy is available."
+  });
+  var new_table = new Happening({
+    "description":"You check out a new table.", 
+    "auto": candy
+  });
+  var throwup = new Happening({
+    "description": "You eat some candy and then throw up.",
+    "effects": {"lose_health": 10},
+    "auto": new_table,
+  });
   candy.choices = [new Choice("Eat some candy", throwup), new Choice("Check out a different table", new_table)];
 
   var state = new_table;
@@ -21,8 +30,8 @@ function Game(){
 
   function build_update(){
     var description = "<div class='phrase'>"+state.description+"</div>"
-    if(state.side_effects["lose_health"]){
-      character.health -= state.side_effects["lose_health"];
+    if(state.effects["lose_health"]){
+      character.health -= state.effects["lose_health"];
     }
     if(state.auto){
       state = state.auto;
@@ -50,11 +59,11 @@ function Character(name){
   this.health = 0;
 }
 
-function Happening(description, choices, side_effects = {}, auto=null){
-  this.description = description;
-  this.choices = choices;
-  this.side_effects = side_effects;
-  this.auto = auto;
+function Happening(options){
+  this.description = options["description"] || "I AM ERROR";
+  this.choices = options["choices"] || [];
+  this.effects = options["effects"] || {};
+  this.auto = options["auto"] || null;
 }
 
 function Choice(text, target){
