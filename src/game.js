f978d159d0a110561b3d98781b8500e68b4bcfc6
GlyphGryph <glyphgryph@gmail.com>
2014-06-27 19:20:51 -0700

    Now displays event descriptions, available choices with styling, and auto-follows auto-happening chains.

--- src/game.js
+++ src/game.js
@@ -4,10 +4,28 @@ function Game(){
   var throwup = new Happening("You throw up.", [], new_table);
   candy.choices = [new Choice("Eat some candy", throwup), new Choice("Check out a different table", new_table)];
 
-  var state = candy;
+  var state = throwup;
 
   this.run = function(){
-    jQuery("#description").html(state.description);
+    var update_text = build_update();
+    jQuery("#description").html(update_text);
+  }
+
+  function build_update(){
+    var description = "<div class='phrase'>"+state.description+"</div>"
+    if(state.auto){
+      state = state.auto;
+      return description + build_update();
+    } else {
+      description += "<div class='choices'>";
+      jQuery.each(state.choices, function(index, choice){
+        description += "<div class='choice'>";
+        description += choice.text;
+        description += "</div>";
+      });
+      description += "</div>";
+    }
+    return description;
   }
 }
 
@@ -19,6 +37,7 @@ function Character(){
 function Happening(description, choices, auto=null){
   this.description = description;
   this.choices = choices;
+  this.auto = auto;
 }
 
 function Choice(text, target){
